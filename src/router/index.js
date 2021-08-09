b4e65d46c5a26435508a3ff6ac5575a400aa85bb
antony <tonymanyoma5@gmail.com>
2021-08-08 21:56:33 -0500

    ajustes de diseño

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import VueRouter from 'vue-router'
 
 import Home from '../views/Home.vue'
 import Welcome from '../views/Welcome.vue'
-import Detailcharacter from '../views/Detailcharacter.vue'
 
 Vue.use(VueRouter)
 
@@ -17,20 +16,6 @@ const routes = [
     path: '/home/:id',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/detailcharacter/:id',
-    name: 'Detailcharacter',
-    props: true,
-    component: Detailcharacter
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
